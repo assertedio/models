7cd6496c7fd0b8d2f290144749df36c8c0aba9a2
Eric Hacke <eric@xtct.io>
2020-02-27 12:41:32 -0500

    fix: change frequency to interval

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import {
-  Frequency,
-  FREQUENCY_UNITS,
-  FrequencyInterface,
-  Mocha,
-  MOCHA_UI,
-  MochaInterface,
-  RoutineConfig,
-  RoutineConfigInterface,
-} from './routineConfig';
+import { Interval, INTERVAL_UNITS, IntervalInterface, Mocha, MOCHA_UI, MochaInterface, RoutineConfig, RoutineConfigInterface } from './routineConfig';
 import { ValidatedBase } from './validatedBase';
 
-export { Frequency, RoutineConfig, FREQUENCY_UNITS, MOCHA_UI, ValidatedBase, Mocha, FrequencyInterface, MochaInterface, RoutineConfigInterface };
+export { Interval, RoutineConfig, INTERVAL_UNITS, MOCHA_UI, ValidatedBase, Mocha, IntervalInterface, MochaInterface, RoutineConfigInterface };
