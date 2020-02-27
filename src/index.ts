79d51e0c426714b75b890138a0e744b1ac0a8c8c
Eric Hacke <eric@xtct.io>
2020-02-27 11:07:53 -0500

    fix: expose frequency class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,13 @@
-import { FREQUENCY_UNITS, FrequencyInterface, Mocha, MOCHA_UI, MochaInterface, RoutineConfig, RoutineConfigInterface } from './routineConfig';
+import {
+  Frequency,
+  FREQUENCY_UNITS,
+  FrequencyInterface,
+  Mocha,
+  MOCHA_UI,
+  MochaInterface,
+  RoutineConfig,
+  RoutineConfigInterface,
+} from './routineConfig';
 import { ValidatedBase } from './validatedBase';
 
-export { RoutineConfig, FREQUENCY_UNITS, MOCHA_UI, ValidatedBase, Mocha, FrequencyInterface, MochaInterface, RoutineConfigInterface };
+export { Frequency, RoutineConfig, FREQUENCY_UNITS, MOCHA_UI, ValidatedBase, Mocha, FrequencyInterface, MochaInterface, RoutineConfigInterface };
