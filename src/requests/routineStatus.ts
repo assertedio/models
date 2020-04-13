99511071879f048eafa0abb663c36bd9b3d5182a
Eric Hacke <eric@roleup.io>
2020-04-12 21:16:52 -0400

    fix: add create for routinestatus

--- src/requests/routineStatus.ts
+++ src/requests/routineStatus.ts
@@ -1,11 +1,13 @@
 import { IsString } from 'class-validator';
 
+import { RoutineInterface } from '../models';
 import { SummaryResult, SummaryResultConstructorInterface, SummaryResultInterface } from './summaryResult';
 import { TIMELINE_EVENT_STATUS } from './timelineEvent';
 
 export enum ROUTINE_CONFIG_STATUS {
   DISABLED = 'disabled',
   NOT_PUSHED = 'notPushed',
+  NO_RECORDS = 'noRecords',
 }
 
 export type OVERALL_ROUTINE_STATUS = ROUTINE_CONFIG_STATUS | TIMELINE_EVENT_STATUS;
@@ -34,6 +36,26 @@ export class RoutineStatus extends SummaryResult implements RoutineStatusInterfa
     if (validate) {
       this.validate();
     }
+  }
+
+  /**
+   * Create instance
+   * @param {RoutineInterface} routine
+   * @param {SummaryResultInterface} status
+   * @returns {RoutineStatus}
+   */
+  static create(routine: RoutineInterface, status: SummaryResultInterface): RoutineStatus {
+    let overallStatus;
+
+    if (!routine.hasPackage) {
+      overallStatus = ROUTINE_CONFIG_STATUS.NOT_PUSHED;
+    } else if (!routine.enabled) {
+      overallStatus = ROUTINE_CONFIG_STATUS.DISABLED;
+    } else {
+      overallStatus = status.latestStatus?.status || ROUTINE_CONFIG_STATUS.NO_RECORDS;
+    }
+
+    return new RoutineStatus({ ...status, overallStatus });
   }
 
   @IsString()
