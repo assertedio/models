bd71ef872e33c98d0f3f06cefc21d617580ce508
Eric Hacke <eric@roleup.io>
2020-04-18 16:13:10 -0400

    fix: add end to pagination

--- src/requests/pagination.ts
+++ src/requests/pagination.ts
@@ -1,19 +1,21 @@
 import { IsDate, IsInt, IsOptional, Max, Min } from 'class-validator';
+import Err from 'err';
+import HTTP_STATUS from 'http-status';
 
 import { toDate } from '../utils';
 import { ValidatedBase } from '../validatedBase';
 
 export interface PaginationInterface {
   start: Date | null;
+  end: Date | null;
   limit: number;
 }
 
 export interface PaginationConstructorInterface {
   start?: Date | string | number;
+  end?: Date | string | number;
   limit?: number;
 }
-
-export type CompletePaginationInterface = Required<PaginationInterface>;
 
 /**
  * @class
@@ -32,7 +34,12 @@ export class Pagination extends ValidatedBase implements PaginationInterface {
     super();
 
     this.start = params.start ? toDate(params.start) : null;
+    this.end = params.end ? toDate(params.end) : null;
     this.limit = params.limit || Pagination.CONSTANTS.DEFAULT_LIMIT;
+
+    if (this.start && this.end) {
+      throw new Err('do not provide both start and end for pagination', HTTP_STATUS.BAD_REQUEST);
+    }
 
     if (validate) {
       this.validate();
@@ -42,6 +49,10 @@ export class Pagination extends ValidatedBase implements PaginationInterface {
   @IsOptional()
   @IsDate()
   start: Date | null;
+
+  @IsOptional()
+  @IsDate()
+  end: Date | null;
 
   @Min(0)
   @Max(Pagination.CONSTANTS.MAX_LIMIT)
--- tests/requests/pagination.unit.ts
+++ tests/requests/pagination.unit.ts
@@ -7,18 +7,33 @@ describe('pagination unit tests', () => {
     const pagination = new Pagination({});
     const expected = {
       start: null,
+      end: null,
       limit: 20,
     };
     expect(pagination).to.eql(expected);
   });
 
-  it('create with everything', () => {
+  it('create with start', () => {
     const pagination = new Pagination({
       start: '2018-01-01T00:00:00.000Z',
       limit: 90,
     });
     const expected = {
       start: new Date('2018-01-01T00:00:00.000Z'),
+      end: null,
+      limit: 90,
+    };
+    expect(pagination).to.eql(expected);
+  });
+
+  it('create with end', () => {
+    const pagination = new Pagination({
+      end: '2018-01-01T00:00:00.000Z',
+      limit: 90,
+    });
+    const expected = {
+      end: new Date('2018-01-01T00:00:00.000Z'),
+      start: null,
       limit: 90,
     };
     expect(pagination).to.eql(expected);
@@ -26,5 +41,11 @@ describe('pagination unit tests', () => {
 
   it('exceed limits', () => {
     expect(() => new Pagination({ limit: 10000 })).to.throw('limit must not be greater than 100');
+  });
+
+  it('start and end', () => {
+    expect(() => new Pagination({ start: '2018-01-01T00:00:00.000Z', end: '2018-01-01T00:00:00.000Z' })).to.throw(
+      'do not provide both start and end for pagination'
+    );
   });
 });
