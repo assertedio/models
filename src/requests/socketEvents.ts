97c32bf5cad611746605c084a82406918c83892b
Eric Hacke <eric@roleup.io>
2020-04-25 15:25:57 -0400

    fix: notify config socket event

--- src/requests/socketEvents.ts
+++ src/requests/socketEvents.ts
@@ -3,4 +3,5 @@ export enum SOCKET_EVENTS {
   RUN_COMPLETE = 'run-complete',
   TIMELINE_UPSERT = 'timeline-upsert',
   BUCKET_UPSERT = 'bucket-upsert',
+  UPSERT_NOTIFY_CONFIG = 'upsert-notify-config',
 }
