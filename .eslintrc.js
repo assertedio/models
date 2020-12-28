0b0a16ce0a972d040989bdffc1bd01990bb56072
Eric Hacke <eric.hacke@searchspring.com>
2020-12-28 17:26:05 -0500

    build: update linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,3 @@
 module.exports = {
-  extends: ['plugin:@typescript-eslint/recommended', '@ehacke/eslint-config', 'plugin:import/typescript'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'import', 'simple-import-sort'],
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    'jsdoc/check-param-names': 'off',
-    'unicorn/expiring-todo-comments': 'error',
-  },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts', '.json'],
-      },
-      typescript: {},
-    },
-    jsdoc: {
-      mode: 'typescript',
-    },
-  },
+  extends: ['@ehacke/eslint-config-ts'],
 };
